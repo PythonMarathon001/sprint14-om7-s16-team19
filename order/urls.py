797507a6d93bb30ede5479a4d102d6dfcc8e9063
kotlyarevskiy.v <48545383+kotlyarevskiy@users.noreply.github.com>
2022-03-30 17:48:47 +0300

    first commit

---
+++ order/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # path('admin/',  admin.site.urls),
+    # path('',        views.index, name='index'),
+    # path('user/',   include("authentication.urls"), name="user"),
+    # path('author/', include("author.urls"), name="author"),
+    # path('book/',   include("book.urls"), name="book"),
+    # path('order/',  include("order.urls"), name="order"),
+]
