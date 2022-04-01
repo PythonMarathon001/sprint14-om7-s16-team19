33d89cb370addef7888b36c7bc0645b0b7b9075d
Dima Evpak <valadimirovich@gmail.com>
2022-04-01 20:49:53 +0300

    Minor tweaks

--- library/views.py
+++ library/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 def index(request):
 
-    return render(request, 'index.html', {'header': 'Test header', 'content_title': 'Test content title', 'content': 'Test content'})
+    return render(request, 'index.html', {'title': 'Library',
+                                         'header': 'Test header', 
+                                         'content_title': 'Test content title', 
+                                         'content': 'Test content'})
