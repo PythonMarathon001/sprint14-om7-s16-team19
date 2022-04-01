d3b5cb466be582bb7f45ddb02929b7116fc0cdc8
Dima Evpak <valadimirovich@gmail.com>
2022-04-01 21:10:08 +0300

    List of all books added

--- book/views.py
+++ book/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from book.models import *
 
 # Create your views here.
 
-def book(request):
-    pass
+def index(request):
+    
+    return render(request, 'book/index.html', {'title': 'Book...'})
+
+
+def list_all(request):
+    content = Book.get_all()
+    return render(request, 'book/list.html', {'title': 'List', 'content_title': 'List of all books' ,'content': content})
