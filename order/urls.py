0ed5108c82d476e3406fc78aae2f57c0796213cd
kotlyarevskiy.v <48545383+kotlyarevskiy@users.noreply.github.com>
2022-04-03 15:50:17 +0300

    orders update - deleted index page

--- order/urls.py
+++ order/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='order'),
-    path('order_list/', order_list, name='order_list')
+    path('', order_list, name='order'),
+    #path('order_list/', order_list, name='order_list')
 ]
