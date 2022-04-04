236f6f081e4567873afb7a27b2cf712d349fb2a3
kotlyarevskiy.v <48545383+kotlyarevskiy@users.noreply.github.com>
2022-04-04 11:21:29 +0300

    main page and some other

--- library/views.py
+++ library/views.py
@@ -3,13 +3,16 @@ from django.shortcuts import render
 
 def index(request):
 
-    return render(request, 'index.html', {'title': 'Library',
-                                         'header': 'Test header', 
-                                         'content_title': 'Test content title', 
-                                         'content': 'Test content'})
+    return render(request, 'index.html', {'title': 'Головна', 
+                                         'content_title': 'Громадська бібліотека ім. Т.Г. Шевченко.', 
+                                         })
     
 def statistics(request):
-    return render(request, "statistics.html")
+    return render(request, "statistics.html", {'title': 'Статистика', 
+                                         'content_title': 'Адміністрування бібліотеки / Правила та нормативні документи', 
+                                         })
 
 def rules(request):
-    return render(request, "rules.html")
+    return render(request, "rules.html", {'title': 'Правила', 
+                                         'content_title': 'Адміністрування бібліотеки / Статистика та звіти', 
+                                         })
