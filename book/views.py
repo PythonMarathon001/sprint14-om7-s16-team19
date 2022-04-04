5a2576e108f2aacdb7838b3e8556780b535c3372
Dima Evpak <valadimirovich@gmail.com>
2022-04-04 21:41:58 +0300

    Search by name

--- book/views.py
+++ book/views.py
@@ -24,7 +24,10 @@ class BookListSearch(ListView):
         self.count = 'none'
         self.authors = list(Author.objects.all())
         self.users = list(CustomUser.objects.all())
-        
+        self.book_info = ""
+        self.author_info = ""
+        self.search_en = False
+        self.q_filter = {}
         data = request.GET
         
         if 'author_id' in data and not data['author_id'] == "0":
@@ -44,6 +47,16 @@ class BookListSearch(ListView):
             self.user_filter['user__pk'] = self.user_id
             user_books_idlist = [order.book_id for order in list(Order.objects.filter(**user_filter))]
             self.filter['id__in'] = user_books_idlist
+        #
+        if 'search_en' in data and data['search_en'] == 'on':
+            self.search_en = data['search_en']
+            self.author_info = data['author_info']
+            self.book_info = data['book_info']
+            self.q_filter = (Q(name__icontains=self.book_info) |\
+                          Q(authors__name__icontains = self.author_info) | \
+                          Q(authors__surname__icontains = self.author_info) | \
+                          Q(authors__patronymic__icontains = self.author_info) )
+        #
         else:
             if 'book_id' in data and not data['book_id'] == "":
                 self.book_id = data['book_id']
@@ -76,12 +89,21 @@ class BookListSearch(ListView):
                                 'author_id': self.author_id, 
                                 'user_id': self.user_id
                                 }
+
+        context['search_param'] = {
+                                'author_info': self.author_info,
+                                'book_info': self.book_info,
+                            }
+        context['search_en'] = self.search_en
    
         return context
    
     def get_queryset(self):
         
-        queryset = Book.get_all_ordered(self.order_by, self.filter)
+        if self.search_en == 'on':
+            queryset = Book.objects.filter(self.q_filter)
+        else:
+            queryset = Book.get_all_ordered(self.order_by, self.filter)
 
         return queryset  
 
@@ -135,75 +157,3 @@ def lookup(request):
             # author_ids = Author.objects.filter(Q(name__contains = data['searching'])| Q(surname__contains = data['searching'])| Q(patronymic__contains = data['searching'])).values_list('id')
             list_of_books = Book.objects.filter(Q(authors__name__contains = data['searching'])| Q(authors__surname__contains = data['searching'])| Q(authors__patronymic__contains = data['searching']))
     return render(request, 'book/list.html', {'header': f'List of books by {data["mode"]}', 'content': list_of_books})
-
-# def book_list(request):
-    
-#     filter  = {}
-#     order_by = []
-#     book_id = ""
-#     book_name = ""
-#     author_id = 0
-#     user_id = 0
-#     name = 'none'
-#     count = 'none'
-#     authors = list(Author.objects.all())
-#     users = list(CustomUser.objects.all())
-    
-#     if request.method == 'GET':
-        
-#         data = request.GET
-                
-#         if 'author_id' in data and not data['author_id'] == "0":
-#             author_id = int(data['author_id'])
-#             filter['authors__id'] = author_id
-            
-#         if 'user_id' in data and not data['user_id'] == "0":
-#             user_filter = {}
-#             user_id =  int(data['user_id'])
-#             if 'book_id' in data and not data['book_id'] == "":
-#                 book_id =  int(data['book_id'])
-#                 user_filter['book__pk'] = book_id
-#             if 'book_name' in data and not data['book_name'] == "":
-#                 book_name = data['book_name']
-#                 user_filter['book__name__icontains'] = book_name              
-                           
-#             user_filter['user__pk'] = user_id
-#             user_books_idlist = [order.book_id for order in list(Order.objects.filter(**user_filter))]
-#             filter['id__in'] = user_books_idlist
-#         else:
-#             if 'book_id' in data and not data['book_id'] == "":
-#                 book_id = data['book_id']
-#                 filter['id'] = book_id
-#             if 'book_name' in data and not data['book_name'] == "":
-#                 book_name = data['book_name']
-#                 filter['name__icontains'] = book_name
-                   
-#         if 'name' in data and not data['name'] == 'none':
-#             name = data['name']
-#             order_by.append('name' if name == 'asc' else '-name')
-        
-#         if 'count' in data and not data['count'] == 'none':
-#             count = data['count']
-#             order_by.append('count' if count == 'asc' else '-count')
-        
-#     content = Book.get_all_ordered(order_by, filter)    
-    
-#     return render(request, 
-#                   'book/list_part2.html', 
-#                   {
-#                       'title': 'List', 
-#                       'content_title': 'List of all books', 
-#                       'content': content, 
-#                       'authors':authors, 
-#                       'users':users, 
-#                       'filters': {
-#                           'book_id': book_id,
-#                           'book_name': book_name, 
-#                           'author_id': author_id, 
-#                           'user_id': user_id
-#                           },
-#                       'orders_by': {
-#                           'name': name, 
-#                           'count': count
-#                           }
-#                       })
