aca1d94083f8b5f783e377ffe3d87ee21c5a5012
kotlyarevskiy.v <48545383+kotlyarevskiy@users.noreply.github.com>
2022-04-04 11:06:38 +0300

    front and back for Author

--- author/urls.py
+++ author/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', author, name='author'),
+    path('', AuthorList.as_view(), name='author'),
 
 ]
--- author/views.py
+++ author/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from order.models import Author
 
-# Create your views here.
-def author(request):
-    pass
+class AuthorList(ListView):
+    
+    model = Author
+    template_name = "author/index.html"
+    context_object_name = "authors"
+    
+
+    
+    def get_context_data(self, **kwargs):
+      
+        context = super(AuthorList, self).get_context_data(**kwargs)
+        context['title'] = 'Список авторів'
+        context['content_title'] = 'Адміністрування бібліотеки / Автори книг'
+           
+        return context
+   
+    def get_queryset(self):
+        
+        queryset = Author.get_all()
+
+        return queryset    
